981cbffbd6bb46803049b92bbcadefcf6b413a86
Rohan <rhnmht30@gmail.com>
2019-04-20 20:19:51 +0530

    user login token route changed from get to post

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -6,7 +6,7 @@ const userController = require("../../../controllers/user_controller");
 const verifyAuth = require("../../../config/jwt");
 
 //generate token to store in browser when user try to login everytime
-router.get("/loginTokenGenerate", userController.generateAndSendToken);
+router.post("/loginTokenGenerate", userController.generateAndSendToken);
 
 //protect routes via token verfication
 router.get("/protected", verifyAuth.receiveAndVerifyToken, (req, res) => {
