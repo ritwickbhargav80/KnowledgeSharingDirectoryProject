30d95d4a95bb459119c02023914faaba5b45e546
Rohan <rhnmht30@gmail.com>
2019-04-28 23:29:10 +0530

    changing Date.now() to Date.now for mongoose to call

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const CommentsSchema = mongoose.Schema({
   for: { type: String, required: true },
   comment: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  date: { type: Date, default: Date.now() }
+  date: { type: Date, default: Date.now }
 });
 
 module.exports = Comment = mongoose.model("Comments", CommentsSchema);
