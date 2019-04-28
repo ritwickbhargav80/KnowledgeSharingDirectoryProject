4f8df75344fd0a61961a95c118b7a4b7a53b38ab
Rohan <rhnmht30@gmail.com>
2019-04-29 02:00:47 +0530

    contact route update: added validations for name, email, phone used REgEx

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -4,10 +4,31 @@ module.exports.index = (req, res) => {
 
 module.exports.sendmessage = (req, res) => {
   let { name, email, phone, message } = req.body;
-  Message.create(req.body, (err, done) => {
-    if (err) throw err;
-    else {
-      req.json({ message: "Your message has been submitted successfully." });
+  console.log(req.body);
+  if (name !== "" || email !== "" || phone !== "" || message !== "") {
+    const nameRegEx = /[A-Za-z]/;
+    const emailRegEx = /\S+@\S+/;
+    if (nameRegEx.test(String(name))) {
+      if (emailRegEx.test(String(email).toLowerCase())) {
+        if (String(phone).length === 10) {
+          Message.create(req.body, (err, done) => {
+            if (err) throw err;
+            else {
+              res.json({
+                message: "success"
+              });
+            }
+          });
+        } else {
+          res.json({ message: "error in phone" });
+        }
+      } else {
+        res.json({ message: "error in email" });
+      }
+    } else {
+      res.json({ message: "error in name" });
     }
-  });
+  } else {
+    res.json({ message: "error" });
+  }
 };
